e02fe6093b9d3ba8d1dc787b7b5f7f54c7fa4389
K. Marple <taeshinoberu@outlook.com>
2022-07-14 14:48:25 -0700

    preliminary constants and main

---
+++ tanks/__main__.py
@@ -0,0 +1,10 @@
+from game.director.director import Director
+from game.director.scene_manager import SceneManager
+
+
+def main():
+    director = Director(SceneManager.VIDEO)
+    director.start_game()
+
+if __name__ == "__main__":
+    main()
