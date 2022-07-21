d6e097a60f69247451c87847b104c10b881b1dff
K. Marple <taeshinoberu@outlook.com>
2022-07-20 21:35:46 -0700

    fix

--- tanks/game/script/load_assets_action.py
+++ tanks/game/script/load_assets_action.py
@@ -9,6 +9,6 @@ class LoadAssetsAction(Action):
         self._video = video
 
     def execute(self, cast, script, callback):
-        self._audio.load_sounds("tank/assets/sounds")
-        self._video.load_fonts("tank/assets/fonts")
-        self._video.load_images("tank/assets/images")
+        self._audio.load_sounds("tanks/assets/sounds")
+        self._video.load_fonts("tanks/assets/fonts")
+        self._video.load_images("tanks/assets/images")
